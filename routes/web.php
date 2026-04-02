1ee567e9abfbd38f36e80de582dfaca37ea23d1a
Carlos Alognon <carlosalognon08@gmail.com>
2026-04-02 20:21:55 +0100

    employee

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AppController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AppController::class, 'index'])->name('home');
@@ -17,15 +18,10 @@ Route::middleware('auth')->group(function () {
     Route::prefix('dashboard')->group(function () {
         Route::get('/', [AppController::class, 'dashboard'])->name('dashboard');
 
-        Route::get('/employees', fn() => inertia('employees/index'))->name('employees.index');
-        Route::get('/employees/create', fn() => inertia('employees/create'))->name('employees.create');
+        Route::resource('employees', EmployeeController::class);
 
         Route::middleware('admin')->group(function () {
-            Route::get('/departments/create', [DepartmentController::class, 'create'])->name('departments.create');
-            Route::get('/departments/{department}/edit', [DepartmentController::class, 'edit'])->name('departments.edit');
-            Route::post('/departments', [DepartmentController::class, 'store'])->name('departments.store');
-            Route::patch('/departments/{department}', [DepartmentController::class, 'update'])->name('departments.update');
-            Route::delete('/departments/{department}', [DepartmentController::class, 'destroy'])->name('departments.destroy');
+            Route::resource('departments', DepartmentController::class);
         });
 
         Route::middleware(['rh'])->group(function () {
